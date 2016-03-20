f7196b451fdd91466782ac757ef1284c06b680ce
Bushische <bushuev.a.a@list.ru>
2016-03-20 23:33:14 +0400

    Models

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -57,10 +57,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+     'default': {
+         'ENGINE': ''django.db.backends.mysql',
+         'NAME': 'tempdb',
+         'USER': 'root',
+#         'password': '',
+         'HOST': '127.0.0.1',
+#         ''
 }
 
 # Internationalization
